63802a9f9fb941f6cf25222502b123ce1b4bc0af
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-27 22:32:28 +0200

    feat(Login): Init design

--- src/features/auth/login/Index.tsx
+++ src/features/auth/login/Index.tsx
@@ -1,8 +1,33 @@
 import { JSX } from "react"
 
+import { useTranslation } from "react-i18next"
+
+import { Flex } from "@/components/layouts"
+import { Button, Card } from "@/components/ui"
+
 const LoginPage = (): JSX.Element => {
+  const { t } = useTranslation()
+
+  const handleSubmit = () => {
+    console.warn("Sbmit")
+  }
+
   return (
-    <div>LoginPage</div>
+    <Flex justify="center" align="center" className="w-100">
+      <Card
+        title={t("Auth.Title")}
+        className="mt-5 mx-2 w-100 fade-in login-container"
+      >
+        {/* Submit */}
+        <Button
+          type="submit"
+          text={t("Auth.Submit")}
+          variant="warning"
+          onClick={handleSubmit}
+          block
+        />
+      </Card>
+    </Flex>
   )
 }
 
