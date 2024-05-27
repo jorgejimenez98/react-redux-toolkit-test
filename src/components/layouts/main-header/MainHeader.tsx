5c9f80be7ed9e452dac40d8c5ab0660baa0d2984
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-27 21:53:11 +0200

    feat(layouts): Updated styles for header

--- src/components/layouts/main-header/MainHeader.tsx
+++ src/components/layouts/main-header/MainHeader.tsx
@@ -1,8 +1,20 @@
 import React from "react"
 
+import { useTranslation } from "react-i18next"
+
+import { Flex } from "../"
+
+import { Logo } from "@/components/common"
+
 const MainHeader: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
-    <header>MainHeader</header>
+    <header>
+      <Flex align="center" justify="between" className="w-100 mt-1">
+        <Logo text={t("General.MainHeader")} />
+      </Flex>
+    </header>
   )
 }
 
--- src/components/layouts/public-header/PublicHeader.tsx
+++ src/components/layouts/public-header/PublicHeader.tsx
@@ -1,8 +1,20 @@
 import React from "react"
 
+import { useTranslation } from "react-i18next"
+
+import { Flex } from "../"
+
+import { Logo } from "@/components/common"
+
 const PublicHeader: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
-    <header>Public Header</header>
+    <header>
+      <Flex align="center" justify="center" className="w-100 mt-1">
+        <Logo text={t("General.PublicHeader")} />
+      </Flex>
+    </header>
   )
 }
 
