e19f3b6460a5008a1bf3e0de624d9fd918346c6f
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-27 20:51:29 +0200

    feat(router): Protect router with navigations to login

--- src/components/common/router/Router.tsx
+++ src/components/common/router/Router.tsx
@@ -1,9 +1,26 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 
+import { PAGE_URLS } from "@/lib/constants"
 import { pagesRoutes } from "@/router"
+import { selectCurrentUser } from "@/stores/slices"
+import { useAppSelector } from "@/stores/store"
 
 const Router: React.FC = () => {
-  const aplicationRoutes = pagesRoutes.map(route => {
+  const user = useAppSelector(selectCurrentUser)
+
+  const applicationRoutes = pagesRoutes.map(route => {
+    // If user is not authenticated, redirect to Login page
+
+    if (route?.isProtected && !user) {
+      return (
+        <Route
+          key={route.title}
+          path={route.path}
+          element={<Navigate to={PAGE_URLS.LOGIN} />}
+        />
+      )
+    }
+
     return (
       <Route
         key={route.title}
@@ -13,7 +30,7 @@ const Router: React.FC = () => {
     )
   })
 
-  return <Routes>{aplicationRoutes}</Routes>
+  return <Routes>{applicationRoutes}</Routes>
 }
 
 export default Router
