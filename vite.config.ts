0ab91f5336c38a7cbb90e835adb42196811ae443
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-27 16:26:07 +0200

    feat(config): Added import alias to vite app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+import path from "path"
+
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Import Alias
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+
 
   // CSS config
   css: {
