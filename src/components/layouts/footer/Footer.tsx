e900e26816e28fa5f49e1fc7fa46f6a1e18804c1
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-27 21:05:52 +0200

    feat(app): Update footer styles

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -1,9 +1,23 @@
 import React from "react"
 
+import { useTranslation } from "react-i18next"
+
+import Flex from "../flex/Flex"
+
+import { Text } from "@/components/ui"
+
 const Footer: React.FC = () => {
+  const { t } = useTranslation()
+
+  const currentYear = new Date().getFullYear()
+
   return (
-    <footer className="mt-auto bg-light py-3">
-        Footer
+    <footer className="mt-auto bg-light py-4">
+      <Flex justify="center" align="center">
+        <Text type="muted">
+          {t("General.Footer", { currentYear })}
+        </Text>
+      </Flex>
     </footer>
   )
 }
